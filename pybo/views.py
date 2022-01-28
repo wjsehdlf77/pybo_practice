f97bbf0f81352e63fe8bcdf7adaf992230456088
wjsehdlf77 <wjsehdlf77@naver.com>
2022-01-28 16:17:53 +0900

    Bootstrap

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,6 @@
 from webbrowser import get
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
-from .models import Question
+from .models import Question, Answer
 from django.utils import timezone
 
 
@@ -32,8 +32,26 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),
-                    create_date=timezone.now())                     #insert해라
-    return redirect('pybo:detail', question_id=question.id)
+    # content = request.POST['content'] 이건 예외발생
+    # content = request.POST.get('content')  #이건 없으면 none을 리턴 두번째인자를 정해주면 정해진값을 리턴
+    # question.answer_set.create(content=request.POST.get('content'),
+    #                 create_date=timezone.now())                     #insert해라
+
+
+    # return redirect('pybo:detail', question_id=question.id)
 
 #answer_set fk설정 부모 모델에 자동으로 생성
+
+# #view함수가 해야될 역할
+# #Answer생성
+#     answer = Answer(question = question,
+#             content = content,
+#             create_date = timezone.now())
+#     answer.save()
+
+#방법2
+    question.answer_set.create(content=request.POST.get('content'),
+                        create_date=timezone.now()) 
+
+    return redirect('pybo:detail', question_id=question.id)
+
