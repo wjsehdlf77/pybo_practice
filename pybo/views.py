44d7f7bb72064527acb5c3983094684bc57860e7
wjsehdlf77 <wjsehdlf77@naver.com>
2022-01-28 20:32:22 +0900

    마무리

--- pybo/views.py
+++ pybo/views.py
@@ -12,6 +12,7 @@ def index(request):
     question_list = Question.objects.order_by('create_date')
     #print(question_list)
     total_count = Question.objects.count()
+    
     context = {
         'question_list' : question_list,
         'total_count' : total_count
